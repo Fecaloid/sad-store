a9d47f518188c0d96b086bb574980563e2786ef7
Artem Bilash <artem.bilash@casafari.com>
2022-10-03 18:20:21 +0300

    info pages

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from apps.main.mixins import DetailViewBreadcrumbsMixin
+from apps.main.models import Page
 
-# Create your views here.
+
+class PageView(DetailViewBreadcrumbsMixin):
+    model = Page
+    template_name = 'main/page.html'
+
+    def get_queryset(self):
+        queryset = Page.objects.filter(is_active=True)
+        return queryset
+
+    def set_breadcrumbs(self):
+        breadcrumbs = {'current': self.object.name}
+        return breadcrumbs
